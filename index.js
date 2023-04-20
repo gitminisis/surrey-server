b39433476228dd46ca52eb7c6b563c7566677cd4
opacteam <mike@minisisinc.com>
2023-04-19 22:00:47 -0700

    update master

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const pagesRouter = require("./pages");
 const summaryRouter = require("./summary");
 const cors = require("cors");
 const app = express();
-
+const port = process.env.PORT || 3001;
 dotenv.config();
 
 app.use(express.json());
@@ -15,42 +15,44 @@ app.use("/summary", summaryRouter);
 
 const baseDir = process.env.BASE_DIR;
 const path = "wwwroot/src/templates/json";
-
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 app.post("/:id", (req, res) => {
-  let data = req.body;
-  let fileName = req.params.id;
-  const filePath = `${baseDir}${path}/${fileName}.json`;
-  console.log(data);
-  fs.writeFile(filePath, JSON.stringify(data), (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log("Data has been written to file successfully.");
-  });
-  res.send("update successful");
+    let data = req.body;
+    let fileName = req.params.id;
+    const filePath = `${baseDir}${path}/${fileName}.json`;
+    console.log(data);
+    fs.writeFile(filePath, JSON.stringify(data), (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("Data has been written to file successfully.");
+    });
+    res.send("update successful");
 });
 
 app.get("/:id", (req, res) => {
-  let fileName = req.params.id;
-  console.log(fileName);
-  const filePath = `${baseDir}${path}/${fileName}.json`;
-  console.log(filePath);
-  if (!filePath) {
-    res.status(400).send("Path parameter is missing");
-    return;
-  }
-
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error reading file");
-      return;
+    let fileName = req.params.id;
+    console.log(fileName);
+    const filePath = `${baseDir}${path}/${fileName}.json`;
+    console.log(filePath);
+    if (!filePath) {
+        res.status(400).send("Path parameter is missing");
+        return;
     }
-    res.send(JSON.parse(data));
-  });
+
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error reading file");
+            return;
+        }
+        res.send(JSON.parse(data));
+    });
 });
 
-const port = 3001;
+
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
