09c8b416f034147c92a50c19b1804af2fe9d3235
opacteam <mike@minisisinc.com>
2023-11-14 14:56:33 -0800

    add ModeToggle in Charkra, cleaned up shadcn

--- src/app/components/ModeToggle.tsx
+++ src/app/components/ModeToggle.tsx
@@ -2,39 +2,19 @@
 
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { IconButton, useColorMode } from "@chakra-ui/react";
 
-export function ModeToggle() {
-  const { setTheme } = useTheme();
+const ModeToggle = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <IconButton
+      aria-label="theme toggle"
+      icon={colorMode === "light" ? <Moon /> : <Sun />}
+      onClick={toggleColorMode}
+    />
   );
-}
+};
+
+export default ModeToggle;
