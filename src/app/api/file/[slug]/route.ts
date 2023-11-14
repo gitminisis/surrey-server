32de8bad0cc0140e79b61a3688f277ee70ce3b66
opacteam <mike@minisisinc.com>
2023-11-14 14:12:34 -0800

    added get and post function api

--- src/app/api/file/[slug]/route.ts
+++ src/app/api/file/[slug]/route.ts
@@ -5,19 +5,40 @@ export async function GET(
   request: NextRequest,
   { params }: { params: { slug: string } }
 ) {
-  const slug = params.slug; // 'a', 'b', or 'c'
+  const fileName = params.slug;
 
-  if (slug.trim() === "") {
+  if (fileName.trim() === "") {
     return NextResponse.error();
   }
 
   try {
     const file = await fs.readFile(
-      `${BASE_DIRECTORY}/json/${slug}.json`,
+      `${BASE_DIRECTORY}/json/${fileName}.json`,
       "utf-8"
     );
     const content = JSON.parse(file);
     return NextResponse.json({ content });
+  } catch (error) {
+    return NextResponse.error();
+  }
+}
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  const fileName = params.slug;
+  if (fileName.trim() === "") {
+    return NextResponse.error();
+  }
+  const data = await request.formData();
+  const content = data.get("data");
+  try {
+    const file = await fs.writeFile(
+      `${BASE_DIRECTORY}/json/${fileName}.json`,
+      content as string
+    );
+    return NextResponse.json({ content: data.get("data") });
   } catch (error) {
     return NextResponse.error();
   }
